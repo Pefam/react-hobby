76a0d8887df2f9901cedb8691b38524e414a284e
M <michael@pefam.com>
2024-08-10 14:41:17 -0600

    separate components created

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
 function App() {
     return (
         <BrowserRouter>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
             </Routes>
         </BrowserRouter>
-    )
-}
-
-function Home() {
-    return (
-        <h1>Hello, React Router!</h1>
-    )
-}
-
-function About() {
-    return (
-        <h1>About page goes here!</h1>
     )
 }
 
