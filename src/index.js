90ed69b65e27c4e57f980ec18bc79d449e7ce277
M <michael@pefam.com>
2024-08-10 14:18:24 -0600

    Two routes working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
     return (
-        <h1>Hello world!</h1>
+        <h1>Hello, React Router!</h1>
+    )
+}
+
+function About() {
+    return (
+        <h1>About page goes here!</h1>
     )
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <App />
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
     </BrowserRouter>
 );
