a7f3f9f04440b963a00eb10b999cc04cc9a05b38
M <michael@pefam.com>
2024-08-14 15:45:20 -0600

    teacher nav also styled

--- src/components/TeacherLayout.js
+++ src/components/TeacherLayout.js
@@ -1,12 +1,28 @@
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 export default function TeacherLayout() {
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
     return (
         <>
         <nav className="teacher-nav">
-            <Link to="/teacher">Dashboard</Link>
-            <Link to="/teacher/income">Income</Link>
-            <Link to="/teacher/reviews">Reviews</Link>
-
+                <NavLink
+                    to="/teacher"
+                    end
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >Dashboard</NavLink>
+            <NavLink
+                    to="/teacher/income"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >Income</NavLink>
+                <NavLink 
+                to="/teacher/reviews"
+                    style={({isActive}) => isActive ? activeStyles : null}
+                >Reviews</NavLink>
         </nav>
         <Outlet />
         </>
