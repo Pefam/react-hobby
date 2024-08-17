18f1c46ec791313edf5eb41330977e9ca79f3e01
M <michael@pefam.com>
2024-08-16 21:06:48 -0600

    sub nav

--- src/pages/Teacher/TeacherHobbyDetail.js
+++ src/pages/Teacher/TeacherHobbyDetail.js
@@ -1,8 +1,14 @@
 import React from "react"
-import { useParams, Link, Outlet } from "react-router-dom"
+import { useParams, Link, NavLink, Outlet } from "react-router-dom"
 export default function TeacherHobbyDetail() {
     const { id } = useParams()
     const [currentHobby, setCurrentHobby] = React.useState(null)
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
 
     React.useEffect(() => {
         fetch(`/api/teacher/hobbies/${id}`)
@@ -35,6 +41,32 @@ export default function TeacherHobbyDetail() {
                         <h4>${currentHobby.price}/day</h4>
                     </div>
                 </div>
+
+                <nav className="teacher-hobby-detail-nav">
+                    <NavLink
+                        to="."
+                        end
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Details
+                    </NavLink>
+
+                    <NavLink
+                        to="pricing"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Pricing
+                    </NavLink>
+
+                    <NavLink
+                        to="photos"
+                        style={({ isActive }) => isActive ? activeStyles : null}
+                    >
+                        Photos
+                    </NavLink>
+
+                </nav>
+
                 <Outlet />
             </div>
         </section>
