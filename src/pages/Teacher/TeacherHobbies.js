d6050f797cec5e55959a0b44ba45633515f2ccc2
M <michael@pefam.com>
2024-09-19 14:52:04 -0600

    defer, Await, Suspense in teacher hobbies

--- src/pages/Teacher/TeacherHobbies.js
+++ src/pages/Teacher/TeacherHobbies.js
@@ -1,41 +1,51 @@
 import React from "react"
-import { Link, useLoaderData } from "react-router-dom"
+import { Link, useLoaderData, defer, Await } from "react-router-dom"
 import { getTeacherHobbies } from "../../api"
 import { requireAuth } from "../../utils"
 
 export async function loader({ request }) {
-    console.log("is it this one?")
     await requireAuth({ request })
-    return getTeacherHobbies()
+    return defer({ hobbies: getTeacherHobbies() })
 }
 
 export default function TeacherHobbies() {
-    const hobbies = useLoaderData()
+    const dataPromise = useLoaderData()
 
-    const teacherHobbiesEls = hobbies.map(hobby => (
-        <Link
-            to={hobby.id}
-            key={hobby.id}
-            className="teacher-hobby-link-wrapper"
-        >
-            <div className="teacher-hobby-single" key={hobby.id}>
-                <img src={hobby.imageUrl} alt={hobby.name} />
-                <div className="teacher-hobby-info">
-                    <h3>{hobby.name}</h3>
-                    <p>${hobby.price}/day</p>
+    function renderHobbyElements(hobbies) {
+        const teacherHobbiesEls = hobbies.map(hobby => (
+            <Link
+                to={hobby.id}
+                key={hobby.id}
+                className="teacher-hobby-link-wrapper"
+            >
+                <div className="teacher-hobby-single" key={hobby.id}>
+                    <img src={hobby.imageUrl} alt={hobby.name} />
+                    <div className="teacher-hobby-info">
+                        <h3>{hobby.name}</h3>
+                        <p>${hobby.price}/day</p>
+                    </div>
                 </div>
-            </div>
-        </Link>
-    ))
+            </Link>
+        ))
+        return (
+            < div className="teacher-hobbies-list" >
+                <section>
+                    {teacherHobbiesEls}
+                </section>
+            </div >
+        )
+    }
+
+    
 
     return (
         <section>
             <h1 className="teacher-hobbies-title">Your listed hobbies</h1>
-            <div className="teacher-hobbies-list">
-                <section>
-                    {teacherHobbiesEls}
-                </section>
-            </div>
+            <React.Suspense fallback={<h2>Loading hobbies...</h2>}>
+                <Await resolve={dataPromise.hobbies}>
+                    {renderHobbyElements}
+                </Await>
+            </React.Suspense>
         </section>
     )
 }
